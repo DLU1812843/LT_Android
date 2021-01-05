2a0e6faf004a9ce7f76a8bb4ee561d3668b5a8ec
NgocThach <62742480+onelove1816@users.noreply.github.com>
2021-01-05 16:06:08 +0700

    Cap nhat giao dien chuc nang chuyen doi Co so Bin Dec Hex

--- Caculator/app/src/main/java/com/example/caculator/ChuyenDoiHeSoActivity.java
+++ Caculator/app/src/main/java/com/example/caculator/ChuyenDoiHeSoActivity.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChuyenDoiHeSoActivity extends AppCompatActivity {
-    EditText txtInput;
-    Button btnChange;
+    RadioButton rd2to10, rd2to16, rd10to2, rd10to16, rd16to2, rd16to10;
+    Button btnChuyenDoi;
     TextView lblOutput;
+    EditText txtInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +21,22 @@ public class ChuyenDoiHeSoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chuyendoiheso);
         Init();
     }
-    public  void Init()
-    {
-      //  txtInput = findViewById(R.id.txtInput);
-      //  lblOutput = findViewById(R.id.lblOutput);
-        btnChange = findViewById(R.id.btnChange);
-        btnChange.setOnClickListener(new View.OnClickListener() {
+    public  void Init() {
+        rd2to10 = findViewById(R.id.rdBinToDec);
+        rd2to16 = findViewById(R.id.rdBinToHex);
+        rd10to2 = findViewById(R.id.rdDecToBin);
+        rd10to16 = findViewById(R.id.rdDecToHex);
+        rd16to2 = findViewById(R.id.rdHexToBin);
+        rd16to10 = findViewById(R.id.rdHexToDec);
+        btnChuyenDoi = findViewById(R.id.btnChuyenDoi);
+        txtInput = findViewById(R.id.txtNhapInput);
+        lblOutput = findViewById(R.id.lblOutput);
+
+        btnChuyenDoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int decimal = Integer.parseInt(txtInput.getText().toString()) ;
-                lblOutput.setText(Integer.toBinaryString(decimal));
+                if (rd2to10.isChecked()==true)
+                {}
             }
         });
 
